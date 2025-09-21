c85725890adff3879f84ed772d0d857ec3a4cba2
Johntor12 <arkanantafirjatullahariza@mail.ugm.ac.id>
2025-09-21 18:46:13 +0700

    fix: add change secret key

--- application/usecases/user_services.py
+++ application/usecases/user_services.py
@@ -13,7 +13,7 @@ from schemas.user_schema import UserCreate
 
 oauth2_scheme = HTTPBearer()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "backend_BISACARE")  # default if not set
+SECRET_KEY = os.getenv("SECRET_KEY", "smart-assurance-with-ai")  # default if not set
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
