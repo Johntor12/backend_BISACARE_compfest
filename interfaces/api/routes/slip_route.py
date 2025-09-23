a552cffe6b68b999390a963b712dabb248c0e5e5
Johntor12 <arkanantafirjatullahariza@mail.ugm.ac.id>
2025-09-23 10:34:30 +0700

    fix: double session in 1 time

--- interfaces/api/routes/slip_route.py
+++ interfaces/api/routes/slip_route.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from infrastructure.db.connection import get_db
 from application.usecases.slip_services import SlipService
 from schemas.slip_schema import SlipCreate, SlipUpdate, SlipResponse
-from application.usecases.user_services import UserService
+from application.usecases.user_services import UserService, get_current_user_service
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 router = APIRouter()
@@ -17,22 +17,22 @@ async def get_current_user_id(
     user = await user_service.get_current_user(credentials)
     return user.id
 
-@router.post("/", response_model=SlipResponse)
-async def create_slip(data: SlipCreate, db: AsyncSession = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    return await SlipService(db).create_slip(data, user_id)
+@router.post("/", response_model=SlipResponse, status_code=status.HTTP_201_CREATED)
+async def create_slip(data: SlipCreate, db: AsyncSession = Depends(get_db),current_user: int = Depends(get_current_user_service)):
+    return await SlipService(db).create_slip(data, current_user)
 
 @router.get("/{slip_id}", response_model=SlipResponse)
-async def get_slip(slip_id: int, db: AsyncSession = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    return await SlipService(db).get_slip(slip_id, user_id)
+async def get_slip(slip_id: int, db: AsyncSession = Depends(get_db),current_user: int = Depends(get_current_user_service)):
+    return await SlipService(db).get_slip(slip_id, current_user)
 
 @router.get("/", response_model=list[SlipResponse])
-async def get_user_slips(db: AsyncSession = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    return await SlipService(db).get_user_slips(user_id)
+async def get_user_slips(db: AsyncSession = Depends(get_db),current_user: int = Depends(get_current_user_service)):
+    return await SlipService(db).get_user_slips(current_user)
 
 @router.put("/{slip_id}", response_model=SlipResponse)
-async def update_slip(slip_id: int, data: SlipUpdate, db: AsyncSession = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    return await SlipService(db).update_slip(slip_id, data, user_id)
+async def update_slip(slip_id: int, data: SlipUpdate, db: AsyncSession = Depends(get_db),current_user: int = Depends(get_current_user_service)):
+    return await SlipService(db).update_slip(slip_id, data, current_user)
 
 @router.delete("/{slip_id}")
-async def delete_slip(slip_id: int, db: AsyncSession = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    return await SlipService(db).delete_slip(slip_id, user_id)
+async def delete_slip(slip_id: int, db: AsyncSession = Depends(get_db),current_user: int = Depends(get_current_user_service)):
+    return await SlipService(db).delete_slip(slip_id, current_user)
